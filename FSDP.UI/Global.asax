a61df6818ac4f200f781be7da4b28f1db99c90af
Mary Frey <mfrey2011@gmail.com>
2017-12-04 09:19:07 -0600

    Initial commit - converted mvc template

---
+++ FSDP.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FSDP.UI.MvcApplication" Language="C#" %>
---
+++ FSDP.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSDP.UI.Startup))]
+namespace FSDP.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
