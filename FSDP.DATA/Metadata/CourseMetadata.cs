bb9f3767c891b33a64760ce51f40ba80774cc7b2
Mary Frey <mfrey2011@gmail.com>
2017-12-04 14:34:07 -0600

    Added metadata for Lesson and Course classes

---
+++ FSDP.DATA/Metadata/CourseMetadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDP.DATA
+{
+    class CourseMetadata
+    {
+        [Required(ErrorMessage = "* Required")]
+        [StringLength(200, ErrorMessage = "* Not to exceed 200 characters")]
+        [Display(Name = "Name")]
+        public string CourseName { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        [UIHint("MultilineText")]
+        [Display(Name = "Description")]
+        public string CourseDescription { get; set; }
+
+        [Display(Name = "Active?")]
+        public bool IsActive { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        public string CourseImage { get; set; }
+    }
+
+    [MetadataType(typeof(CourseMetadata))]
+    public partial class Course { }
+}
---
+++ FSDP.DATA/Metadata/LessonMetadata.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDP.DATA
+{
+    class LessonMetadata
+    {
+        [Required(ErrorMessage ="* Required")]
+        [Display(Name ="Title")]
+        [StringLength(200, ErrorMessage ="* Not to exceed 200 characters")]
+        public string LessonTitle { get; set; }
+
+        [Required(ErrorMessage = "* Required")]
+        [Display(Name ="Course ID")]
+        public int CourseID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        [StringLength(300, ErrorMessage ="* Not to exceed 300 characters")]
+        public string Introduction { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        [StringLength(250, ErrorMessage = "* Not to exceed 250 characters")]
+        [Display(Name = "Video URL")]
+        public string VideoUrl { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        [StringLength(100, ErrorMessage = "* Not to exceed 100 characters")]
+        [Display(Name = "PDF File Name")]
+        public string PdfFileName { get; set; }
+
+        [Display(Name = "Active?")]
+        public bool IsActive { get; set; }
+
+        [DisplayFormat(NullDisplayText = "N/A")]
+        [Display(Name = "Image")]
+        public string LessonImage { get; set; }
+    }
+}
