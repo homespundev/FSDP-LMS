3825497dcf2f7cdc9d3b7af7ff962c6314ea8cf6
Mary Frey <mfrey2011@gmail.com>
2017-12-06 16:31:06 -0600

    Successfully added email capabilities via contact form. Did not successfully implement a working map

--- FSDP.UI/Controllers/HomeController.cs
+++ FSDP.UI/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using FSDP.UI.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
 
 namespace FSDP.UI.Controllers
 {
@@ -12,16 +15,48 @@ namespace FSDP.UI.Controllers
         [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
-
+            
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel contact)
+        {
+            string body = string.Format("Name: {0}<br/>Email: {1}<br/>Subject: {2}<br/>Message: {3}",
+                contact.Name,
+                contact.Email,
+                contact.Subject,
+                contact.Message);
+
+            MailMessage msg = new MailMessage(
+                "no-reply@homespundev.com"
+                , "mfrey2011@gmail.com",
+                contact.Subject,
+                body);
+            msg.IsBodyHtml = true;
+            msg.Priority = MailPriority.High;
+
+            SmtpClient client = new SmtpClient("mail.homespundev.com");
+            client.Credentials = new NetworkCredential("no-reply@homespundev.com", "P@ssw0rd");
+            using (client)
+            {
+                try
+                {
+                    client.Send(msg);
+                }
+                catch
+                {
+                    ViewBag.ErrorMessage = "There was an error sending your email. Please try again.";
+                    return View();
+                }
+            }
+            return View("ContactConfirmation", contact);
         }
     }
 }
