af903ed93b98cac3e7d20d2fc2b019658abef98f
Mary Frey <mfrey2011@gmail.com>
2017-12-13 00:29:23 -0600

    added google login, on testing, create for classes and class sign up and edit for courses not working

---
+++ FSDP.DATA/Metadata/StoreClassMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDP.DATA
+{
+    class StoreClassMetadata
+    {
+        [Display(Name = "Class")]
+        public string ClassName { get; set; }
+        [Display(Name = "Description")]
+        public string ClassDescription { get; set; }
+    }
+
+    [MetadataType(typeof(StoreClassMetadata))]
+    public partial class StoreClass { }
+}
